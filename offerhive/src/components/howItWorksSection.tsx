ed6793272aaf301d6ee602c2c182a0c1b7a7c74b
Adeel <adeeltahir6a@gmail.com>
2025-04-20 21:05:20 +0500

    --modified how it works section page--

--- offerhive/src/components/howItWorksSection.tsx
+++ offerhive/src/components/howItWorksSection.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 export default function HowItWorksSection() {
   return (
-<section className="flex flex-col items-center justify-center w-screen md:w-full ">
+<section className="flex flex-col items-center justify-center max-w-screen md:w-full ">
 {/* Decorative Wave SVG */}
       <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
         <path
