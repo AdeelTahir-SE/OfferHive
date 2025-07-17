0611ff2dbb849fe5ca3331c19318b02613eefd11
Adeel <adeeltahir6a@gmail.com>
2025-07-17 17:56:13 +0500

    --fixed signup error--

--- offerhive/src/app/api/signUp/route.ts
+++ offerhive/src/app/api/signUp/route.ts
@@ -1,22 +1,35 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { signUp } from "@/lib/Db2/db";
+
 export async function POST(request: NextRequest) {
   const { email, password } = await request.json();
   const { userData, signUpError, insertError } = await signUp(email, password);
+
   if (signUpError) {
-    return new Response(JSON.stringify({ error: signUpError }), {
-      status: 400,
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json(
+      { error: signUpError },
+      {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
+
   if (insertError) {
-    return new Response(JSON.stringify({ error: insertError }), {
-      status: 500,
-      headers: { "Content-Type": "application/json" },
-    });
+    return NextResponse.json(
+      { error: insertError },
+      {
+        status: 500,
+        headers: { "Content-Type": "application/json" },
+      }
+    );
   }
-  return new Response(JSON.stringify({ userData }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+
+  return NextResponse.json(
+    { userData },
+    {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    }
+  );
 }
